885a46155cf7a77d136e9115cba47291ce7cb752
Abdul Kosimov <abdul.kosimov@student.ap.be>
2025-03-20 07:14:20 +0100

    klaar voor het labo

--- Program.cs
+++ Program.cs
@@ -9,15 +9,22 @@
             student1.Name = "Said Aziz";
             student1.StudentNumber = Student.StudentCounter;
             Student.StudentCounter++; // student aangemaakt, dus verhogen
-            student1.Courses[0] = "Programmeren";
-            student1.Courses[1] = "Databanken";
+
+            // courses zijn private gemaakt, nu zijn ze niet meer accessible
+            //student1.courses[0] = "Programmeren";
+            //student1.courses[1] = "Databanken";
+            student1.RegisterForCourse("Programmeren");
+            student1.RegisterForCourse("Databanken");
+
 
             Student student2 = new Student();
             student2.Birthday = new DateTime(2000, 6, 1);
             student2.Name = "Mieke Vermeulen";
             student2.StudentNumber = Student.StudentCounter;
             Student.StudentCounter++;
-            student2.Courses[0] = "Communicatie";
+
+            //student2.courses[0] = "Communicatie";
+            student2.RegisterForCourse("Communicatie");
 
             Console.WriteLine(student1.GenerateNameCard());
             Console.WriteLine(student2.GenerateNameCard());
--- Student.cs
+++ Student.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +11,7 @@ namespace SchoolAdmin
     {
         public string Name;
         public DateTime Birthday;
-        public string[] Courses = new string[5];
+        private string[] courses = new string[5];
         public uint StudentNumber;
         public static uint StudentCounter;
         // static want, gaat niet over maar 1 student, wel over de klasse Student
@@ -20,9 +21,9 @@ namespace SchoolAdmin
         public double DetermineWorkLoad()
         {
             double total = 0.0;
-            for (int i = 0; i < Courses.Length; i++)
+            for (int i = 0; i < courses.Length; i++)
             {
-                if (Courses[i] != null)
+                if (courses[i] != null)
                 {
                     total+=10;
                 }
@@ -43,26 +44,22 @@ namespace SchoolAdmin
         {
             return $"{this.Name} (STUDENT)";
         }
-
-        public void Quote(byte CourseIndex, byte score)
+        public void RegisterForCourse(string course)
         {
-
+            // binnenin de klasse kan je wel aan de private-fields, buiten de klasse niet
+            int freePlace = -1;
+            // -1, om de lus te beëindigen
+            for (int i = 0; i < courses.Length && freePlace == -1; i++)
+            {
+                if (this.courses[i] is null)
+                {
+                    freePlace = i;
+                }
+            }
+            if(freePlace > -1)
+            {
+                courses[freePlace] = course;
+            }
         }
-
-        public void RegisterForCourse(string courseName)
-        {
-            
-        }
-
-        public void ShowOverview()
-        {
-
-        }
-
-        public void DemonstrateStudent()
-        {
-            
-        }
-
     }
 }
